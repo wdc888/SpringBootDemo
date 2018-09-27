f47f3a42e42a42bf3afba46b0c8e0de64c9e020e
wdc888 <1475706756@qq.com>
2018-09-27 22:34:30 +0800

    Initial commit

---
+++ src/main/java/com/java/springboot/demo/SpringBootDemo.java
@@ -0,0 +1,13 @@
+package com.java.springboot.demo;
+
+/**
+ *  SpringBootDemo案例主类
+ *
+ * @author  wangdongchao 2018/9/27
+ */
+public class SpringBootDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
